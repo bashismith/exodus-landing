ce13f3479a999111754b8e107e287dc6fead7717
Bashiri Smith <bashirismith1419@gmail.com>
2021-07-01 12:56:27 -0700

    pics

--- client/components/GettingStarted.jsx
+++ client/components/GettingStarted.jsx
@@ -13,7 +13,9 @@ const GettingStarted = () => {
       <p className="pcomp">
         Before starting any data migration project, we're going to navigate to
         the directory we want Exodus to use as the
-        current working directory. Config and migration directory/files will be created here. To start the project, type <p className="pcomp"><b> deno run --unstable --allow-read --allow-write --allow-net cli.ts init</b></p> This will create a <b>setup.ts</b> file we will use to configure our mongoDB connection.
+        current working directory. Config and migration directory/files will be created here. To start the project, type
+        <p className="pcomp"><b> deno run --unstable --allow-read --allow-write --allow-net cli.ts init</b></p>
+        This will create a <b>migrations directory</b> and <b>setup.ts</b> file we will use to configure our mongoDB connection.
       </p>
 
       <h2>Setting up the database connection</h2>
@@ -28,29 +30,29 @@ const GettingStarted = () => {
 
       </p>
 
-      <h2>Creating and defining migrations</h2>
+      <h2>Creating and Running Migrations</h2>
       <h3>Creating Migrations</h3>
       <p className="pcomp">
         To create a migration file, we run the following in the command line <p className="pcomp"><b> deno run --unstable --allow-read --allow-write --allow-net cli.ts create [migrationName]</b></p> A file with the name: date and time + the migration title will be created in the migrations folder. For example:  <p className="pcomp"><b> deno run --unstable --allow-read --allow-write --allow-net cli.ts create testmigration</b></p>
         <p className="pcomp">creates a file called <br></br><b>2021_01_01_24_12_60_testmigration</b></p> This is the file we modify to define the migration and you can create multiple
         migration files. Using the file date/time in the file name, Exodus will run the files in the order they were created.
       </p>
-      <h3>Defining Migrations</h3>
+      <h3>The Migration Files</h3>
       <p className="pcomp">
         When you open up the migrations file you just created, it'll
         just be two empty functions: fwd and back. This is where you can define
         the actions of the data migration and the steps to rollback the
-        changes. We'll talk more about how we can interact with MongoDB but first let's look at an example migration with two two migration files below.
+        changes. We'll talk more about how we can interact with MongoDB but first let's look at an example migration with two already defined migration files below.
       </p>
       <h4>Example Migration</h4>
       <p className="pcomp">
-      2021_01_01_24_12_60_testmigration1.ts
+      2021_01_01_24_12_60_testmigration_1.ts
         </p>
 
           <img src={migration1} />
 
           <p className="pcomp">
-      2021_01_02_24_12_60_testmigration2.ts
+      2021_01_02_24_12_60_testmigration_2.ts
         </p>
 
           <img src={migration2} />
@@ -58,36 +60,52 @@ const GettingStarted = () => {
       <p className="pcomp">
 
         <p className="pcomp">
-        In <b>testmigration1</b> fwd has been defined to find all documents with "item : 'boba'" within the food collection, and add a 'price' field with a value of 25.
+        In <b>testmigration_1</b> fwd has been defined to find all documents with "item : 'boba'" within the food collection, and add a 'price' field with a value of 25.
         </p>
 
         <p className="pcomp">
-        In <b>testmigration2</b>, fwd is defined to add a quantity field to all documents with "type : 'doodads'" in the widget collection. Because <b>testmigration1</b> was created first, that will be the first migration run.
+        In <b>testmigration_2</b>, fwd is defined to add a 'quantity' field to all documents with "type : 'doodads'" in the widget collection. Because <b>testmigration_1</b> was created first, that will be the first migration run.
         </p>
 
-         We can choose to run just this migration (fwd), or all migrations (full). Let's go ahead and run all of them. <p className="pcomp"><b> deno run --unstable --allow-read --allow-write --allow-net cli.ts full</b></p>
+         We can choose to run just this migration (fwd), or all migrations (full). Let's go ahead and run all of them with the 'full' command.
+         <p className="pcomp">
+           <b> deno run --unstable --allow-read --allow-write --allow-net cli.ts full</b>
+           </p>
 
-Great! Now all the migrations have been applied.
-        The back function has been defined to roll back the changes in the fwd
-        function, by finding all instances of boba within the food collection
-        and removing the key. Running back in the command line will remove the
-        quanitity key from all instances of boba within the food collection.<br/>
+<p className="pcomp">Great! Now all the migrations have been applied: all the boba in the food collection should now have a price of 25, and all doodads in the widget collection should now have a quantity field. Now how do we reverse these changes? </p>
 
-        <b> Be careful when defining the fwd and back functions. It is up to
+<h3>Rollinging back migrations</h3>
+<p className="pcomp">
+Rolling back the changes is incredibly simple. Just run
+        <p className="pcomp">
+           <b> deno run --unstable --allow-read --allow-write --allow-net cli.ts back</b>
+           </p>
+And it will run the back function defined in <b>testmigration_2</b>. Now all 'quanitity' fields should have been removed from the documents with 'doodads'. Exodus knows where we are in the migration so if we run the same 'back' command again, it will run the back function from <b>testmigration_1</b>. The same applies to the 'fwd' command so you can incrementally run the migration files or roll them back as necessary.
+</p>
+
+<p className="pcomp">
+<b> Be careful when defining the fwd and back functions. It is up to
         you to make sure that the back function is defined properly to rollback
         the changes in the fwd function, or that irreversible changes in the fwd
         function are acceptable to you. For example, if you remove a 'quantity'
         key in the fwd function but define the back function to add it back, the
         'quantity' field will return but the original value of 'quanitity' will
         be lost unless you have written in some way to preserve that value. </b>
+           </p>
+
+
       </p>
+      <h3>Defining Migrations</h3>
+
+      <p className="pcomp">
+        Now that we've seen a migration and rollback in action, let's go back and talk about how to define your migrations
+           </p>
 
       <h4>Available database actions</h4>
       <p className="pcomp">
         Exodus uses deno_mongo for the MongoDB driver (as of now there is no
-        official Mongodb support for Deno), which allows code in the migration
-        files to interact with MongoDB and perform CRUD operations. If you want to access another database without changing the database in the setup file you can use the client object in the argument.The two arguments passed in to each function (client,db) let you
-        perform commands and actions just as you would in a normal mongo shell.
+        official Mongodb support for Deno), which works similarly to the MongoDB node.js driver and MongoDB shell. allows code in the migration
+        files to interact with MongoDB and perform CRUD operations. The two arguments passed in to each function (client,db) let you perform commands and actions just as you would in a normal mongo shell. If you want to access another database without changing the database in the setup file you can use the client object in the argument.
 
       </p>
 
@@ -106,7 +124,7 @@ Great! Now all the migrations have been applied.
       <h5>2. MongoDB Database commands</h5>
       <p className="pcomp">
         If the built-in database and collection methods of deno_mongo are
-        insufficient, you can instead access the <a target='_blank' className='url' href="https://docs.mongodb.com/manual/reference/command/">MongoDB database commands</a> using client.runCommand('databaseName',databaseCommand). Several
+        insufficient, you can instead access the <a target='_blank' className='url' href="https://docs.mongodb.com/manual/reference/command/">MongoDB database commands</a> using client.runCommand('databaseName',databaseCommand). Since this allows you to directly access the MongoDB database commands, you can perform far database actions in addition to CRUD operations. Several
         examples are found below. 1. ping 2. findAndModify 3. count.
       </p>
 
